ee9203494117692771748a433686ddc7f48371d7
Piotrek <piotrekstrumnik@gmail.com>
2019-02-18 17:15:29 +0100

    enable cors

--- src/main/java/pl/piotrek/tenants/config/SwaggerConfig.java
+++ src/main/java/pl/piotrek/tenants/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package pl.piotrek.tenants.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -38,5 +39,10 @@ public class SwaggerConfig extends DelegatingWebMvcConfiguration{
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
