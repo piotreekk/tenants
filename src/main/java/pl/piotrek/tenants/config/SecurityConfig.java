e144787edcb114e37a8d90cf4f58a794fc18383b
Piotrek <piotrekstrumnik@gmail.com>
2019-02-18 18:47:29 +0100

    CORS working with Basic Auth.

--- src/main/java/pl/piotrek/tenants/config/SecurityConfig.java
+++ src/main/java/pl/piotrek/tenants/config/SecurityConfig.java
@@ -1,13 +1,32 @@
 package pl.piotrek.tenants.config;
 
-//@Configuration
-public class SecurityConfig //extends WebSecurityConfigurerAdapter {
-{
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().and().authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .oauth2Login();
-//    }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                    .withUser("user")
+                    .password("{noop}password")
+                    .roles("USER");
+    }
+
+
 }
