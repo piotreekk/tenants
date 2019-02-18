2ed7d3b11dbe893a09d2054001cf49a0e5577045
Piotrek <piotrekstrumnik@gmail.com>
2019-02-18 17:17:16 +0100

    enable cors

--- src/main/java/pl/piotrek/tenants/config/SecurityConfig.java
+++ src/main/java/pl/piotrek/tenants/config/SecurityConfig.java
@@ -9,7 +9,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors().and().authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
