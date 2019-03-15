51d80b1abde9226e28a793688fb41fd7220176e5
Piotrek <piotrekstrumnik@gmail.com>
2019-03-15 23:46:20 +0100

    HouseworkController test added

--- src/test/java/pl/piotrek/tenants/controller/HouseworkControllerTest.java
+++ src/test/java/pl/piotrek/tenants/controller/HouseworkControllerTest.java
@@ -1,13 +1,27 @@
 package pl.piotrek.tenants.controller;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import pl.piotrek.tenants.api.assembler.HouseworkRatingResourceAssembler;
 import pl.piotrek.tenants.api.assembler.HouseworkResourceAssembler;
 import pl.piotrek.tenants.api.mapper.HouseworkMapper;
 import pl.piotrek.tenants.api.mapper.HouseworkRatingMapper;
+import pl.piotrek.tenants.model.entity.Housework;
 import pl.piotrek.tenants.service.HouseworkRatingService;
 import pl.piotrek.tenants.service.HouseworkService;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class HouseworkControllerTest {
     MockMvc mockMvc;
@@ -18,20 +32,51 @@ public class HouseworkControllerTest {
     @Mock
     HouseworkRatingService houseworkRatingService;
 
-    HouseworkResourceAssembler assembler;
+    HouseworkResourceAssembler houseworkAssembler = new HouseworkResourceAssembler();
 
-    HouseworkMapper houseworkMapper;
+    HouseworkMapper houseworkMapper = HouseworkMapper.INSTANCE;
 
-    HouseworkRatingMapper houseworkRatingMapper;
+    HouseworkRatingMapper houseworkRatingMapper = HouseworkRatingMapper.INSTANCE;
 
-    HouseworkRatingResourceAssembler ratingAssembler;
+    HouseworkRatingResourceAssembler ratingAssembler = new HouseworkRatingResourceAssembler();
 
+    HouseworkController houseworkController;
 
+    @Before
     public void setUp(){
-//        MockitoAnnotations.initMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup();
+        MockitoAnnotations.initMocks(this);
+
+        houseworkController = new HouseworkController(houseworkService, houseworkRatingService, houseworkAssembler, houseworkMapper, houseworkRatingMapper, ratingAssembler);
+        mockMvc = MockMvcBuilders.standaloneSetup(houseworkController).build();
 
     }
 
 
+    @Test
+    public void getHouseworkByIdTest() throws Exception {
+        Housework housework = new Housework();
+        housework.setId(1L);
+        housework.setName("Housework");
+
+        when(houseworkService.getHousework(anyLong())).thenReturn(housework);
+
+        mockMvc.perform(get("/api/housework/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", Matchers.equalTo("Housework")));
+
+        verify(houseworkService, times(1)).getAvgRatingForHousework(anyLong());
+    }
+
+
+    @Test
+    public void getAllHouseworksTest() throws Exception {
+        List<Housework> houseworks = List.of(new Housework(), new Housework(), new Housework());
+
+        when(houseworkService.getHouseworksOf(anyLong())).thenReturn(houseworks);
+
+        mockMvc.perform(get("/api/housework/house/1"))
+                .andExpect(status().isOk());
+
+        verify(houseworkService, times(houseworks.size())).getAvgRatingForHousework(anyLong());
+    }
 }
